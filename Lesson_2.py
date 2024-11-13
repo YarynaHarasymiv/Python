97f6b080c19b9b7f789c5fd9b2f84eeee6e2efc7
Ярина <yarunakurash@gmail.com>
2024-11-14 00:50:21 +0200

    Lesson_3.1

--- Lesson_2.py
+++ Lesson_2.py
@@ -1,51 +1,51 @@
-#num_str = str(num_str)
-#print(type(num_str))
-from dis import distb
-
-message = 'Hi, my name is Python!'
-message = message.replace('y', '0')
-message = message.replace('i', '1')
-print(message)
-
-split_test = 'This is a split test'
-split_test = split_test.split()
-print(split_test)
-
-split_test = " ".join(split_test)
-print(split_test)
-
-string_join = len(split_test)
-print(string_join)
-
-string_join = split_test
-print(len(string_join))
-
-
-
-list_append = [1, 2, 3]
-
-list_append.append(4)
-print(list_append)
-
-list_append.append(5)
-print(list_append)
-
-
-list_extend = [4, 5, 6]
-
-list_extend.extend([7, 8, 9])
-print(list_extend)
-
-
-print(list_extend.index(6))
-
-print(len(list_append))
-
-dict_test = {'car': 'Toyota', 'price': '4900', 'where': 'EU'}
-
-print(dict_test['car'], dict_test['where'])
-
-print(dict_test.keys())
-print(dict_test.values())
-
-print(dict_test.items())
+# #num_str = str(num_str)
+# #print(type(num_str))
+# from dis import distb
+#
+# message = 'Hi, my name is Python!'
+# message = message.replace('y', '0')
+# message = message.replace('i', '1')
+# print(message)
+#
+# split_test = 'This is a split test'
+# split_test = split_test.split()
+# print(split_test)
+#
+# split_test = " ".join(split_test)
+# print(split_test)
+#
+# string_join = len(split_test)
+# print(string_join)
+#
+# string_join = split_test
+# print(len(string_join))
+#
+#
+#
+# list_append = [1, 2, 3]
+#
+# list_append.append(4)
+# print(list_append)
+#
+# list_append.append(5)
+# print(list_append)
+#
+#
+# list_extend = [4, 5, 6]
+#
+# list_extend.extend([7, 8, 9])
+# print(list_extend)
+#
+#
+# print(list_extend.index(6))
+#
+# print(len(list_append))
+#
+# dict_test = {'car': 'Toyota', 'price': '4900', 'where': 'EU'}
+#
+# print(dict_test['car'], dict_test['where'])
+#
+# print(dict_test.keys())
+# print(dict_test.values())
+#
+# print(dict_test.items())
---
+++ Lesson_3.1.py
@@ -0,0 +1,102 @@
+# correct_passvord = "password123"
+#
+# user_password = input("Введіть пароль:")
+# if user_password == correct_passvord:
+#     print("Ви увійшли в систему")
+# else:
+#     print("Неправильний пароль")
+from calendar import weekheader
+
+
+
+#
+# # Встановлюємо номер дня тижня
+# day_number = int(input("Введіть номер дня тижня (1-7): "))
+#
+# # Визначаємо назву дня тижня за номером
+# if day_number == 1:
+#     print("Понеділок")
+# elif day_number == 2:
+#     print("Вівторок")
+# elif day_number == 3:
+#     print("Середа")
+# elif day_number == 4:
+#     print("Четвер")
+# elif day_number == 5:
+#     print("П'ятниця")
+# elif day_number == 6:
+#     print("Субота")
+# elif day_number == 7:
+#     print("Неділя")
+# else:
+#     print("Помилка: Невірний номер дня")
+
+
+
+
+#
+# day_1 = {
+#     "day_is": "Monday",
+#     "number_of_day": 1
+# }
+# day_2 = {
+#     "day_is": "Tuesday",
+#     "number_of_day": 2
+# }
+# day_3 = {
+#     "day_is": "Wednesday",
+#     "number_of_day": 3
+# }
+# day_4 = {
+#     "day_is": "Thursday",
+#     "number_of_day": 4
+# }
+# day_5 = {
+#     "day_is": "Friday",
+#     "number_of_day": 5
+# }
+# day_6 = {
+#     "day_is": "Saturday",
+#     "number_of_day": 6
+# }
+# day_7 = {
+#     "day_is": "Sunday",
+#     "number_of_day": 7
+# }
+#
+#
+# number_of_day = [1, 2, 3, 4, 5, 6, 7]
+# list_of_day = [day_1, day_2, day_3, day_4, day_5, day_6, day_7]
+#
+#
+# day_number = 8
+#
+#
+# for day in list_of_day:
+#     if day["number_of_day"] == day_number:
+#         print(day["day_is"], day["number_of_day"])
+#     else:
+#         print("day is wrong")
+
+
+
+# a = 6
+# n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for number in n:
+#     suma = a*number
+#     result = str(number) + " * " + str(a) + "=" + str(suma)
+#     print(result)
+#     # print(number, "*", a, "=", a*number)
+
+# list = []
+#
+# list.append(1)
+# list.append(3)
+# list.append(5)
+#
+# print(list)
+# print(list[2])
+# print(list.index(3))
+
+
+
