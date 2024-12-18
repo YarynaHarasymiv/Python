c92e4ea72eba26668bad49b28ff01e16c3ba0cd9
Ярина <yarunakurash@gmail.com>
2024-12-18 21:59:13 +0200

    finish

--- Lesson.4_17.12.py
+++ Lesson.4_17.12.py
@@ -1,3 +1,5 @@
+#Списки
+
 # a = [1, 2, 3, 4, 5]
 # b = ["apple", "banana", "cherry"]
 #
@@ -68,36 +70,51 @@
 #
 #
 #
+#Кортежі
 #
 #
+# a = (1, 2, 3, 4, 5, 5, 4)
+# print(a[0], a[1], a[2])
+# print(a[:2], a[-2:])
 #
+# print(a.count(5), a.count(4))
+# print(a.index(4))
 
+
+
+
+#Словники
+
+#
+# test_dict = {"user": "Oleg", "age": 21, "country": "Poland"}
+# print(test_dict["user"], test_dict["age"], test_dict.get("country"))
+# print(test_dict.get("animal", "key not found"))
+# test_dict["age"] = 30
+# print(test_dict["age"])
+# test_dict["animal"] = "cat"
+# print(test_dict["animal"])
+# print(test_dict)
+# animal = test_dict.pop("animal")
+# print(test_dict)
+# print(animal)
 
 test_dict = {"user": "Oleg", "age": 21, "country": "Poland"}
-print(test_dict["user"], test_dict["age"], test_dict.get("country"))
-print(test_dict.get("animal", "key not found"))
-test_dict["age"] = 30
-print(test_dict ["age"])
-test_dict[ "animal"] = "cat"
-print(test_dict["animal"])
-animal = test_dict.pop("animal")
-print(animal)
-
 copy_test = test_dict.copy()
 test_dict.clear()
-print(test_dict, "<-copied->", copy_test)
+print(test_dict)
+print(copy_test)
 
 for key, value in copy_test.items():
-    print(f"Key: {key}, Value: {value}")
+    print(f"Key:{key}, Value: {value}")
+# for item in copy_test:
+#     print(item, "------>")
+# for item in copy_test.values():
+#         print(item)
 
-for value in copy_test.values():
-    print(value)
 
 wrong_key = copy_test.pop("currency", "key not found")
 print(wrong_key)
 
-###
 dict_update = {"new_role": "admin", "salary": 10000}
 copy_test.update(dict_update)
 print(copy_test)
-
