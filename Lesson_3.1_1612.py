7e8c4e328fc24a0e3bfe371118a78a73945a5ce6
Ярина <yarunakurash@gmail.com>
2024-12-20 16:58:11 +0200

    Lesson_5

--- Lesson_3.1_1612.py
+++ Lesson_3.1_1612.py
@@ -1,165 +1,165 @@
-# correct_passvord = "password123"
-#
-# user_password = input("Введіть пароль:")
-# if user_password == correct_passvord:
-#     print("Ви увійшли в систему")
-# else:
-#     print("Неправильний пароль")
-#
+correct_passvord = "password123"
 
-
-
-
-# enter_password = "password"
-#
-# correct_password = "password123"
-#
-# if enter_password == correct_password:
-#     print("Ви увійшли в систему")
-# else:
-#     print("Неправтльний пароль")
+user_password = input("Введіть пароль:")
+if user_password == correct_passvord:
+    print("Ви увійшли в систему")
+else:
+    print("Неправильний пароль")
 
 
 
 
 
+enter_password = "password"
 
+correct_password = "password123"
 
-# # Встановлюємо номер дня тижня
-# day_number = int(input("Введіть номер дня тижня (1-7): "))
-#
-# # Визначаємо назву дня тижня за номером
-# if day_number == 1:
-#     print("Понеділок")
-# elif day_number == 2:
-#     print("Вівторок")
-# elif day_number == 3:
-#     print("Середа")
-# elif day_number == 4:
-#     print("Четвер")
-# elif day_number == 5:
-#     print("П'ятниця")
-# elif day_number == 6:
-#     print("Субота")
-# elif day_number == 7:
-#     print("Неділя")
-# else:
-#     print("Помилка: Невірний номер дня")
-
-
-
-
-
-# day_1 = {
-#     "day_is": "Monday",
-#     "number_of_day": 1
-# }
-# day_2 = {
-#     "day_is": "Tuesday",
-#     "number_of_day": 2
-# }
-# day_3 = {
-#     "day_is": "Wednesday",
-#     "number_of_day": 3
-# }
-# day_4 = {
-#     "day_is": "Thursday",
-#     "number_of_day": 4
-# }
-# day_5 = {
-#     "day_is": "Friday",
-#     "number_of_day": 5
-# }
-# day_6 = {
-#     "day_is": "Saturday",
-#     "number_of_day": 6
-# }
-# day_7 = {
-#     "day_is": "Sunday",
-#     "number_of_day": 7
-# }
-#
-#
-# number_of_day = [1, 2, 3, 4, 5, 6, 7]
-# list_of_day = [day_1, day_2, day_3, day_4, day_5, day_6, day_7]
-#
-#
-# day_number = 7
-#
-#
-# for day in list_of_day:
-#     if day["number_of_day"] == day_number:
-#         print(day["day_is"], day["number_of_day"])
-#     else:
-#         print("day is wrong")
-
-
-
-# a = 6
-# n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# for number in n:
-#     suma = a*number
-#     result = str(number) + " * " + str(a) + "=" + str(suma)
-#     print(result)
-#     # print(number, "*", a, "=", a*number)
-
-# n_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# n = int(input("Введіть число (1-10): "))
-# for num in n_1:
-#     suma = n * num
-#     result = str(n) + " * " + str(num) + "=" + str(suma)
-#     print(result)
-
-
-
-# list = []
-#
-# list.append(1)
-# list.append(3)
-# list.append(5)
-#
-# print(list)
-# print(list[2])
-# print(list.index(3))
+if enter_password == correct_password:
+    print("Ви увійшли в систему")
+else:
+    print("Неправтльний пароль")
 
 
 
 
 
 
-#
-# list_of_number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# print("Сума чисел:",sum(list_of_number))
-#
-# sum_of_number = sum(list_of_number)
-# print("Сума чисел:",sum_of_number)
+
+# Встановлюємо номер дня тижня
+day_number = int(input("Введіть номер дня тижня (1-7): "))
+
+# Визначаємо назву дня тижня за номером
+if day_number == 1:
+    print("Понеділок")
+elif day_number == 2:
+    print("Вівторок")
+elif day_number == 3:
+    print("Середа")
+elif day_number == 4:
+    print("Четвер")
+elif day_number == 5:
+    print("П'ятниця")
+elif day_number == 6:
+    print("Субота")
+elif day_number == 7:
+    print("Неділя")
+else:
+    print("Помилка: Невірний номер дня")
 
 
 
-# number = int(input("Введіть число:"))
-#
-# factorial = 1
-# for i in range(1, number + 1):
-#     factorial *= i
-#
-# print(f"Факторіал числа {number} дорівнює:", factorial)
+
+
+day_1 = {
+    "day_is": "Monday",
+    "number_of_day": 1
+}
+day_2 = {
+    "day_is": "Tuesday",
+    "number_of_day": 2
+}
+day_3 = {
+    "day_is": "Wednesday",
+    "number_of_day": 3
+}
+day_4 = {
+    "day_is": "Thursday",
+    "number_of_day": 4
+}
+day_5 = {
+    "day_is": "Friday",
+    "number_of_day": 5
+}
+day_6 = {
+    "day_is": "Saturday",
+    "number_of_day": 6
+}
+day_7 = {
+    "day_is": "Sunday",
+    "number_of_day": 7
+}
+
+
+number_of_day = [1, 2, 3, 4, 5, 6, 7]
+list_of_day = [day_1, day_2, day_3, day_4, day_5, day_6, day_7]
+
+
+day_number = 7
+
+
+for day in list_of_day:
+    if day["number_of_day"] == day_number:
+        print(day["day_is"], day["number_of_day"])
+    else:
+        print("day is wrong")
+
+
+
+a = 6
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for number in n:
+    suma = a*number
+    result = str(number) + " * " + str(a) + "=" + str(suma)
+    print(result)
+    # print(number, "*", a, "=", a*number)
+
+n_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+n = int(input("Введіть число (1-10): "))
+for num in n_1:
+    suma = n * num
+    result = str(n) + " * " + str(num) + "=" + str(suma)
+    print(result)
+
+
+
+list = []
+
+list.append(1)
+list.append(3)
+list.append(5)
+
+print(list)
+print(list[2])
+print(list.index(3))
 
 
 
 
 
-# a = 1
-# b = 50
-#
-# for i in range(1, b + 1):
-#     if i % 2 == 0:
-#         print(i)
+
+
+list_of_number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("Сума чисел:",sum(list_of_number))
+
+sum_of_number = sum(list_of_number)
+print("Сума чисел:",sum_of_number)
 
 
 
-# for num in range(1, 51):
-#     if all(num %i != 0  for i in range(2, num)):
-#         print(num)
+number = int(input("Введіть число:"))
+
+factorial = 1
+for i in range(1, number + 1):
+    factorial *= i
+
+print(f"Факторіал числа {number} дорівнює:", factorial)
+
+
+
+
+
+a = 1
+b = 50
+
+for i in range(1, b + 1):
+    if i % 2 == 0:
+        print(i)
+
+
+
+for num in range(1, 51):
+    if all(num %i != 0  for i in range(2, num)):
+        print(num)
 
 
 
---
+++ Lesson_5.py
@@ -0,0 +1,58 @@
+# def say_hello(username, age):
+#     print(f"Hello {username} welcome to the club, buddy")
+#     print(f"Your age is {age}, you are so beautiful")
+#     print("------->")
+#
+# def print_numbers(start, stop):
+#     for num in range(start, stop):
+#         print(f"Current number is {num}")
+#     print("------>")
+#
+# user_data = {"Dima": 25, "Sarah": 34, "Tom": 11}
+# list_of_ranges = [(1, 10), (2, 9), (0, 10)]
+#
+# # for name, age in user_data.items():
+# #     say_hello(name, age)
+#
+# for start_pos, stop_pos in list_of_ranges:
+#     print_numbers(start_pos, stop_pos)
+import turtle
+
+# print("------")
+# def check_connection(username, count_tries, priority):
+#     if priority >= 10:
+#         finish = 5
+#         for attemt in range(1, count_tries + 1):
+#             if attemt == finish:
+#                 print("Connect was successfully")
+#                 break
+#             print(f"Attemp: {attemt} to connect to {username}")
+#
+#     elif priority >= 5 and priority < 10:
+#         finish = 3
+#         for attemt in range(1, 6):
+#             if attemt == finish:
+#                 print("Connect was successfully")
+#             print(f"Attwmp: {attemt} to connect to username")
+#
+#     else:
+#         print("Your username has so low priority")
+#
+# check_connection(count_tries=10, username="Oleg", priority=100)
+
+import turtle
+
+def drowSquare(size, color):
+    turtle.speed(1)
+    turtle.color(color)
+    turtle.begin_fill()
+    def move(len):
+        turtle.forward(len)
+        turtle.left(90)
+    for _ in range(4):
+            move(size)
+    turtle.end_fill()
+
+drowSquare(100, "red")
+turtle.goto(200, 200)
+drowSquare(200, "blue")
