3acc68cb1a7590471739202f9ebcc7778d3c2b7f
Ярина <yarunakurash@gmail.com>
2024-12-22 13:45:14 +0200

    Lesson_2_Part_2

---
+++ Lesson_5/main.py
@@ -0,0 +1,8 @@
+
+import utilities
+
+
+list_of_number = [1, 2, 3, 4, 5, 15]
+print(f"Середнє значення списку: ", utilities.calculate_average(list_of_number))
+print(f"Найбільше число у списку: ", utilities.find_max(list_of_number))
+
