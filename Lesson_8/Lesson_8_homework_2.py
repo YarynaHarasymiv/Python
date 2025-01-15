0cb0fe663c7397247701b401623d46718096463c
Ярина <yarunakurash@gmail.com>
2025-01-15 15:09:53 +0200

    Lesson_8_homework_part_2

--- Lesson_8/Lesson_8_homework_2.py
+++ Lesson_8/Lesson_8_homework_2.py
@@ -1,4 +1,50 @@
-cla
+from abc import ABC, abstractmethod
 
+
+class Shape(ABC):
+
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+import math
+
+class Circle(Shape):
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calculate_area(self):
+        return math.pi * (self.radius ** 2)
+
+
+class Rectangle(Shape):
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def calculate_area(self):
+        return self.width * self.height
+
+
+
+class Triangle(Shape):
+
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def calculate_area(self):
+        return  0.5 * self.base * self.height
+
+
+circle = Circle(4)
+rectangle = Rectangle(3, 5)
+triangle = Triangle(2, 6)
+
+print(f"Площа кола : {circle.calculate_area()}")
+print(f"Площа прямокутника : {rectangle.calculate_area()}")
+print(f"Площа трикутника : {triangle.calculate_area()}")
 
 
