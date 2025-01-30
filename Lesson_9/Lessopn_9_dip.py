d43a4d8ce2dd852f48c1b6b692e0a40932095321
Ярина <yarunakurash@gmail.com>
2025-01-30 23:47:14 +0200

    Lesson_9_dip

--- Lesson_9/Lessopn_9_dip.py
+++ Lesson_9/Lessopn_9_dip.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from abc import  abstractmethod
+from abc import ABC, abstractmethod
 
 
 class Teams(Enum):
@@ -7,17 +7,36 @@ class Teams(Enum):
     RED_TEAM = 2
     GREEN_TEAM = 3
 
+class Students(Enum):
+    OLEG = 1
+    DIMA = 2
+    SERGEY = 3
+
 
 class TeamMembershipLookUp():
     @abstractmethod
     def find_all_students(self, team):
         pass
 
+class StudentMembershipLookUp(ABC):
+    @abstractmethod
+    def find_all_teams(self, student):
+        pass
+
+
+
 class Student:
     def __init__(self, name):
         self.name = name
 
-class TeamMemberShips:
+class Team:
+    def __init__(self, name_team):
+        self.name_team = name_team
+
+
+
+
+class TeamMemberShips(TeamMembershipLookUp):
     def __init__(self):
         self.team_memberships = []
 
@@ -29,17 +48,35 @@ class TeamMemberShips:
             if members[1] == team:
                 yield  members[0].name
 
-        # student_names = []
-        # for member in self.team_memberships:
-        #     if member[1] == team:
-        #         student_names.append(member[0].name)
-        # return student_names
+        student_names = []
+        for member in self.team_memberships:
+            if member[1] == team:
+                student_names.append(member[0].name)
+        return student_names
+
+class StudentMemberShips(StudentMembershipLookUp):
+    def __init__(self):
+        self.student_memberships = []
+
+    def add_student_memberships(self, student, team):
+        self.student_memberships.append((student, team))
+
+    def find_all_teams(self, student):
+        for teams in self.student_memberships:
+            if teams[0] == student:
+                yield teams[1].name_team
 
 
-class Analysis:
+
+class Analysis_1():
     def __init__(self, team_member_ship_lookup):
         for student in team_member_ship_lookup.find_all_students(Teams.RED_TEAM):
             print(f"{student} is in Red team")
+
+class Analysis_2:
+    def __init__(self, student_member_ship_lookup):
+        for team in student_member_ship_lookup.find_all_teams(Students.OLEG):
+            print(f"Oleg is in a {team} ")
 
 
 
@@ -49,7 +86,19 @@ student_3 = Student("Sergey")
 
 team_memberships = TeamMemberShips()
 team_memberships.add_team_memberships(student_1, Teams.RED_TEAM)
-team_memberships.add_team_memberships(student_2, Teams.RED_TEAM)
-team_memberships.add_team_memberships(student_3, Teams.RED_TEAM)
+team_memberships.add_team_memberships(student_2, Teams.BLUE_TEAM)
+team_memberships.add_team_memberships(student_3, Teams.GREEN_TEAM)
 
-Analysis(team_memberships)
+
+team_1 = Team("BLUE_TEAM")
+team_2 = Team("RED_TEAM")
+team_3 = Team("GREEN_TEAM")
+
+student_membership = StudentMemberShips()
+student_membership.add_student_memberships(Students.DIMA, team_1)
+student_membership.add_student_memberships(Students.OLEG, team_2)
+student_membership.add_student_memberships(Students.SERGEY, team_3)
+
+Analysis_1(team_memberships)
+print("-----")
+Analysis_2(student_membership)
