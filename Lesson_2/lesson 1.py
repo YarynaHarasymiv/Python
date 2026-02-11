11a890a72577739d743852b4f044981d33038180
Ярина <yarunakurash@gmail.com>
2026-02-11 13:38:27 +0200

    Lesson 1.py

---
+++ Lesson_2/lesson 1.py
@@ -0,0 +1,102 @@
+# num_1 = 5
+# print(type(num_1))
+#
+# num_2 = 3.14
+# print(type(num_2))
+#
+# string = "hello"
+# print(type(string))
+#
+# check = True
+# print(type(check))
+#
+# lst = ["hello", "my", "name", "is", "Yaryna"]
+# print(type(lst))
+#
+# tpl = (1, 2, 3)
+# print(type(tpl))
+#
+# dct= {'name':'Yaryna', 'age': 32}
+# print(type(dct))
+#
+# set_ex = {1, 2, 3}
+# print(type(set_ex))
+#
+# print(type(None))
+#
+# class Person:
+#     pass
+# a = Person()
+# print(type(a))
+#
+#
+#
+#
+#
+# lst = [1, 2, 3, 4, 5]
+# dct = {'name': 'Yaryna', 'age': 5}
+# name = 'Yaryna'
+# tpl = ('n', 'a', 'g')
+#
+# print(dct['name'] == name and dct['age'] in lst)
+# result = dct['age'] in lst or dict['name'] in tpl
+# print(result)
+# result = dct['age'] in lst and dict['name'] in tpl
+# print(result)
+#
+#
+#
+# num_1 = '1'
+# print(type(num_1))
+# print(num_1)
+# num1 = int(num_1)
+# print(type(num1))
+# print(num1)
+# num1 = float(num_1)
+# print(type(num1))
+# # print(num1))
+#
+#
+#
+# string = 'hello world!'
+# print(len(string))
+# string = string.upper()
+# print(string)
+# string = string.lower()
+# print(string)
+# string = string.capitalize()
+# print(string)
+# string = string.replace('!', '?')
+# print(string)
+# string = string.split()
+# print(string)
+# string = ' '.join(string)
+# print(string)
+# string = string.count('o')
+# print(string)
+# string = 1
+# string = str(string)
+# print(type(string))
+#
+#
+# base_list = [1, 2, 3]
+# print(len(base_list))
+# base_list.append(4)
+# print(base_list)
+# base_list.extend([5, 6, 7])
+# print(base_list)
+# print(base_list.index(4))
+#
+#
+#
+# base_dict = {'name':'Yaryna', 'age': 32, 'high': 172}
+# print(base_dict.keys())
+# print(base_dict.values())
+# print(base_dict.items())
+# print(base_dict['name'], base_dict.get('is_animal', 'No'))
+#
+#
+#
+#
+#
+#
